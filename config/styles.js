91df553964b6a9c27f37f516d9788c2585d1c365
Arturo Guerrero <aj.gupa@Arturos-MacBook-Pro.local>
2018-03-21 09:51:12 -0600

    Update Login UI

---
+++ config/styles.js
@@ -0,0 +1,8 @@
+module.exports = {
+    mainColor: function() {
+        return 'rgba(254, 225, 51, 1.0)'
+    },
+    titleColor: function() {
+        return 'rgba(35, 35, 35, 1.0)'
+    }
+}
--- screens/LoginView.js
+++ screens/LoginView.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
-import { AppRegistry, View, Button, Text, StyleSheet } from 'react-native';
+import { AppRegistry, View, Text, StyleSheet, TextInput } from 'react-native';
+import Button from 'react-native-button'
 import { NavigationActions } from 'react-navigation';
+import DefaultStyles from '../config/styles'
 
 export default class LoginView extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { emailText: '', passwordText: '', hidePassword: true};
+    }
 
     _login() {
         this.props.navigation.dispatch(
@@ -18,14 +25,29 @@ export default class LoginView extends React.Component {
 
     render() {
         return (
-          <View style={styles.container}>
-              <Text style={styles.bigTitleText}>Welcome to Login!</Text>
-              <Button
-                  styles={styles.bigTitleText}
-                  onPress={() => this._login()}
-                  title = "Login"
-              />
-          </View>
+            <View style={styles.container}>
+                <Text style={styles.bigTitleText}>Enroute</Text>
+                <TextInput
+                    style={styles.emailInput}
+                    placeholder="Email"
+                    onChangeText={(text) => this.setState({emailText: text})}
+                    underlineColorAndroid="transparent"
+                     />
+                <TextInput
+                    style={styles.passwordInput}
+                    placeholder="Password"
+                    secureTextEntry={this.state.hidePassword}
+                    onChangeText={(text) => this.setState({passwordText: text})}
+                    underlineColorAndroid="transparent"
+                     />
+                <Button
+                    containerStyle={styles.loginButtonContainer}
+                    disabledContainerStyle={styles.loginButtonDisabledContainer}
+                    style={styles.loginButtonText}
+                    onPress={() => this._login()}>
+                    Login
+                </Button>
+            </View>
         );
     }
 }
@@ -36,10 +58,42 @@ const styles = StyleSheet.create({
       flexDirection: 'column',
       justifyContent: 'center',
       alignItems: 'center',
+      backgroundColor: DefaultStyles.mainColor()
     },
     bigTitleText: {
       color: 'black',
       fontWeight: 'bold',
       fontSize: 30,
     }, 
+    emailInput: {
+        marginTop: 20,
+        paddingLeft: 10,
+        height: 40, 
+        width: 250, 
+        backgroundColor: 'white', 
+        borderWidth: 1
+    },
+    passwordInput: {
+        marginTop: 10,
+        paddingLeft: 10,
+        height: 40, 
+        width: 250, 
+        backgroundColor: 'white', 
+        borderWidth: 1
+    },
+    loginButtonContainer: {
+        margin: 10,
+        height: 40, 
+        width: 250, 
+        borderRadius:4, 
+        backgroundColor: 'grey'
+    },
+    loginButtonDisabledContainer: {
+        backgroundColor: 'grey'
+    },
+    loginButtonText: {
+        fontSize: 20,
+        color: 'white',
+        paddingTop: 5,
+    }
   });
