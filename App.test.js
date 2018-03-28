375d0b0d22b5bc67a767d463c23fefa4bddbcfaa
Arturo Guerrero <aj.gupa@Arturos-MacBook-Pro.local>
2018-03-28 11:48:01 -0600

    Fix test failing on png issue

--- App.test.js
+++ App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import App from './App';
+import LoginView from './screens/LoginView';
 
 import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
-  const rendered = renderer.create(<App />).toJSON();
+  const rendered = renderer.create(<LoginView />).toJSON();
   expect(rendered).toBeTruthy();
 });
